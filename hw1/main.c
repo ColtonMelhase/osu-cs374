6ac75ac3a2171f740cc6399f03859dce11adf049
Colton Melhase <melhasec@os1.engr.oregonstate.edu>
2024-01-10 00:04:33 -0800

    Add processFile and print functions

--- hw1/main.c
+++ hw1/main.c
@@ -8,6 +8,47 @@ struct movie {
     struct movie *next;
 };
 
+struct movie *processFile(char *filePath) {
+
+    FILE *movieFile = fopen(filePath, "r");
+
+    char *currLine = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    char *token;
+
+    struct movie *head = NULL;
+    struct movie *tail = NULL;
+
+    getline(&currLine, &len, movieFile);
+    while((nread = getline(&currLine, &len, movieFile)) != -1) {
+        struct movie *newNode = createMovie(currLine);
+
+        if(head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    free(currLine);
+    fclose(movieFile);
+    return head;
+}
+
+void printMovie(struct movie *aMovie) {
+    printf("%s, %d\n", aMovie->title, aMovie->year);
+}
+
+void printMovieList(struct movie *list) {
+    while(list != NULL) {
+        printMovie(list);
+        list = list->next;
+    }
+}
 int main(int argc, char *argv[]) {
-    
+    struct movie *list = processFile(argv[1]);
+    printMovieList(list);
 }
