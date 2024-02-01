848a0b9d54faa67b79f6d2a008c50644a6cfa67d
Colton Melhase <coltonmelhase12@gmail.com>
2024-02-01 15:56:19 -0800

    Add skeleton for 3 process functions

--- hw2/main.c
+++ hw2/main.c
@@ -8,6 +8,19 @@
 #include <string.h>
 #include <stdbool.h>
 
+bool processLargest() {
+
+}
+
+bool processSmallest() {
+
+}
+
+bool processSpecific() {
+    
+}
+
+
 void displayMainMenu() {
     printf("\n1. Select file to process\n"
             "2. Exit the program\n\n"
@@ -21,47 +34,49 @@ void displayFileMenu() {
             "Enter 2 to pick the smallest file\n"
             "Enter 3 to specify the name of a file\n\n"
 
-            "Enter a choice from 1 to 3:");
+            "Enter a choice from 1 to 3: ");
 }
 
 int main() {
     int choiceMain = -1;
-
+    int choiceFile = -1;
+    bool fileProcessed = false;
     do {
         displayMainMenu();
-        choiceMain = -1;
-        scanf("%d", choiceMain);
+        // choiceMain = -1;
+        scanf("%d", &choiceMain);
 
         switch(choiceMain) {
             case 1:
-                bool fileProcessed = false;
+                
                 do {
                     displayFileMenu();
-                    int choiceFile = -1;
-                    scanf("%d", choiceFile);
+                    
+                    // choiceFile = -1;
+                    scanf("%d", &choiceFile);
 
                     switch(choiceFile) {
                         
                         case 1:
-                            printf("\nChoice 1");
+                            printf("\n\tChoice 1\n");
                             fileProcessed = true;
                             break;
                         case 2:
-                            printf("\nChoice 2");
+                            printf("\n\tChoice 2\n");
                             fileProcessed = true;
                             break;
                         case 3:
-                            printf("\nChoice 3");
+                            printf("\n\tChoice 3\n");
                             fileProcessed = true;
                             break;
                         default:
-                            printf("\nThe file null was not found. Try again");
+                            printf("\n\tThe file null was not found. Try again\n");
                             break;
                     }
                 } while (!fileProcessed);
                 break;
             case 2:
-                printf("\nQuitting...");
+                printf("\nQuitting...\n");
                 exit(0);
                 break;
         }
