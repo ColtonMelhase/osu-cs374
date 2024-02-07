0a3389ed9a5c7de9e5a3247601a6b719a0bf8f6c
Colton Melhase <coltonmelhase12@gmail.com>
2024-02-07 07:43:20 -0800

    Add comment and blank line functionality

--- hw3/main.c
+++ hw3/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 void sh_exit() {
-    exit();
+    exit(1);
 }
 
 void sh_cd() {
@@ -15,4 +18,18 @@ void sh_status() {
 
 int main() {
 
+	// allocate memory to store command. Command line must
+	// support max length of 2048. Thus 2049 bytes given. (+1 for \0)
+	char* userCommand = malloc(sizeof(char) * 2049);
+
+	while(1) { // run until exit command
+		printf(": "); fflush(stdout); // prompt
+		fgets(userCommand, sizeof(char) * 2049, stdin);	// get user input
+
+		if(userCommand[0] == '#' || userCommand[0] == '\n') { // if command is blank or a comment
+			// do nothing
+		} else {
+			printf("%s", userCommand); fflush(stdout);
+		}
+	}
 }
